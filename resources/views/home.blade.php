f8023aa62377e24638bfd624a568eca5a29e435d
ItzArima <skaypaprilia@gmail.com>
2022-01-11 13:53:08 +0100

    base project structure with a controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('CSS')
+<link rel="stylesheet" href="{{asset('css/home.css')}}">
+
+@section('content')
+    <main>
+        <h1>THIS IS THE HOME PAGE</h1>
+    </main>
+@endsection
